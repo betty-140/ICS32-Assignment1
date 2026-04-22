0911b2c32a0a7a6e4e8a96aacdc7596a712175c8
betty-140 <runmengl@uci.edu>
2026-04-22 14:12:41 -0700

    fix the error

--- a1.py
+++ a1.py
@@ -1,3 +1,13 @@
+# a1.py
+
+# Starter code for assignment 1 in ICS 32 Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# Betty Liu
+# runmengl@uci.edu
+# 12306049
+
 from pathlib import Path
 import shlex
 
